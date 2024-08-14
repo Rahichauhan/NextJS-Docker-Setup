7107293ec1a33e644d58b8358f2f8857ed0ca06a
Rahichauhan <rahichauhan36@gmail.com>
2024-08-14 08:05:41 +0530

    Next and Docker

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+   async function getData() {
+    const res=await fetch(`${process.env.API_URL}posts`,{cache:"no-store"});
+    if(!res.ok) return notFound();
+    return res.json();
+    
+   }
+export default async function Home() {
+const data=await getData();
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-2xl font-bold">Data List</h1>
+      <ul>
+        {data.map((item:any)=>(
+          < li key={item.id} className="mb-5">
+            <h2>{item.title}</h2>
+            <p>User Id:{item.userId}</p>
+            <p>Id:{item.id}</p>
+            <p>Description:{item.body}</p>
+
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
